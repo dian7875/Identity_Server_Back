c6105c0651b8c1630743c539ad695147b6b5059f
Akion <akion.cheng.jimenez@est.una.ac.cr>
2024-10-04 23:59:07 -0600

    Implementación de jwt y identity server como en la presenación

---
+++ Identity Server Backend/Config/ApiResources.cs
@@ -0,0 +1,13 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+public static class ApiResources
+{
+    public static IEnumerable<ApiResource> Get()
+    {
+        return new List<ApiResource>
+        {
+            new ApiResource("api1", "My API")
+        };
+    }
+}
